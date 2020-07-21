2ab9c60a0c020f5d2c9b7b7d5edf9278b44c9f08
mociofiletto <mociofiletto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2020-07-21 08:25:25 +0000

    tagging version 1.1.2
    git-svn-id: http://plugins.svn.wordpress.org/campi-moduli-italiani/tags/1.1.2@2343946 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ modules/comune/class-gcmi-comune-shortcode.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Wordpress shortcode for italian municipality seect cascade
+ *
+ * Adds a shortcode that generates a cascade of select to choose an Italian municipality
+  *
+ * @link https://wordpress.org/plugins/search/campi+moduli+italiani/
+ *
+ * @package campi-moduli-italiani
+ * @subpackage comune
+ * @since 1.0.0
+ */
+ 
+class GCMI_COMUNE_ShortCode extends GCMI_COMUNE {
+	private $kind;
+	private $comu_details;
+	private $id;
+	private $name;
+	private $class;
+
+	function __construct( $atts ) {
+		if ( ! parent::is_valid_kind( $atts['kind'] ) ) {
+			$this->kind = 'tutti';
+		} else {
+			$this->kind = $atts['kind'];
+		}
+		$this->name  = sanitize_html_class( $atts['name'] );
+		$this->class = sanitize_html_class( $atts['class'] );
+		if ( preg_match( '/^[a-zA-Z][\w:.-]*$/', $atts['id'] ) ) {
+			$this->id = $atts['id'];
+		}
+		$this->comu_details = ( $atts['comu_details'] === true ? true : false );
+	}
+
+	public function get_html() {
+
+		parent::gcmi_comune_enqueue_scripts();
+
+		$regioni = $this->gcmi_start( $this->kind );
+		$MyIDs   = parent::getIDs( $this->id );
+		$uno     = '<div class="gcmi_wrap">';
+		$uno    .= '<p>' . __( 'Select a region:', 'campi-moduli-italiani' ) . '<br />';
+		$uno    .= '<select name="' . $this->name . '_IDReg" id="' . $MyIDs['reg'] . '" class = "' . $this->class . '" >';
+		$uno    .= '<option value="">' . __( 'Select...', 'campi-moduli-italiani' ) . '</option>';
+		foreach ( $regioni as $val ) {
+			$uno .= '<option value="' . $val['i_cod_regione'] . '">' . $val['i_den_regione'] . '</option>';
+		}
+		$uno .= '</select></p>';
+
+		$due  = '<p>' . __( 'Select a province:', 'campi-moduli-italiani' ) . '<br />';
+		$due .= '<select name="' . $this->name . '_IDPro" id="' . $MyIDs['pro'] . '" class = "' . $this->class . '">';
+		$due .= '<option value="">' . __( 'Select...', 'campi-moduli-italiani' ) . '</option>';
+		$due .= '</select></p>';
+
+		$tre = '<p>' . __( 'Select a municipality:', 'campi-moduli-italiani' ) . '<br />';
+
+		$tre .= '<select name="' . $this->name . '" id="' . $MyIDs['com'] . '" class = "' . $this->class . '">';
+		$tre .= '<option value="">' . __( 'Select...', 'campi-moduli-italiani' ) . '</option>';
+		$tre .= '</select>';
+		if ( $this->comu_details ) {
+			$tre .= '<img src="' . plugin_dir_url( GCMI_PLUGIN ) . '/img/gcmi_info.png" width="30" height="30" id="' . $MyIDs['ico'] . '" style="vertical-align: middle; margin-top: 10px; margin-bottom: 10px; margin-right: 10px; margin-left: 10px;">';
+		}
+		$tre     .= '</p>';
+		$quattro  = '<input type="hidden" name="' . $this->name . '_kind" id="' . $MyIDs['kin'] . '" value="' . $this->kind . '" />';
+		$quattro .= '<input type="hidden" name="' . $this->name . '_targa" id="' . $MyIDs['targa'] . '"/>';
+		$quattro .= '<input class="comu_mail" type="hidden" name="' . $this->name . '_formatted" id="' . $MyIDs['form'] . '"/>';
+
+		// these fields are useful if you use key/value pairs sent by the form to generate a PDF - from 1.1.1
+		$quattro .= '<input type="hidden" name="' . $this->name . '_reg_desc" id="' . $MyIDs['reg_desc'] . '"/>';
+		$quattro .= '<input type="hidden" name="' . $this->name . '_prov_desc" id="' . $MyIDs['prov_desc'] . '"/>';
+		$quattro .= '<input type="hidden" name="' . $this->name . '_comu_desc" id="' . $MyIDs['comu_desc'] . '"/>';
+
+		$quattro .= '</div>';
+		if ( $this->comu_details ) {
+			$quattro .= '<span id="' . $MyIDs['info'] . '" title="' . __( 'Municipality details', 'campi-moduli-italiani' ) . '"></span>';
+		}
+		$html = '<span class="gcmi_wrap">' . $uno . $due . $tre . $quattro . '</span>';
+		return $html;
+	}
+}
+
